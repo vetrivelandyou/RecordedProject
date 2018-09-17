9331645b2d1215a3063b3c628eb6be63304ed09a
Vetricel D <vetrivel.dhandapani@gmail.com>
2018-09-17 07:36:47 +0530

    Added MVC UserRegistration Project

---
+++ MvcUserRegistration/UserRegistration/Controllers/UserController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using UserRegistration.Models;
+
+namespace UserRegistration.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult AddOrEdit(int id = 0)
+        {
+            User user = new User();
+            return View(user);
+        }
+        [HttpPost]
+        public ActionResult AddOrEdit(User user)
+        {
+            using (DBModels db = new DBModels ())
+            {
+                if (db.Users.Any(x => x.UserName == user.UserName))
+                {
+                    ViewBag.DuplicateMessage = "User Name Already Exists.";
+                    return View("AddOrEdit", user);
+                }
+                else
+                {
+                    db.Users.Add(user);
+                    db.SaveChanges();
+                }
+            }
+            ModelState.Clear();
+            ViewBag.SuccessMessage = "Registration Successful.";
+            return View("AddOrEdit",new User());
+        }
+    }
+}
---
+++ MvcUserRegistration/UserRegistration/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="UserRegistration.MvcApplication" Language="C#" %>
